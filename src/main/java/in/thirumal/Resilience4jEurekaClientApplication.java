8596c8f75cc6849c8c3095221a6691d58740d7fe
Thirumal <m.thirumal@hotmail.com>
2020-03-04 21:16:24 +0530

    first commit

---
+++ src/main/java/in/thirumal/Resilience4jEurekaClientApplication.java
@@ -0,0 +1,13 @@
+package in.thirumal;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Resilience4jEurekaClientApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Resilience4jEurekaClientApplication.class, args);
+	}
+
+}
